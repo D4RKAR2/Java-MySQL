2694b632f339c14d69d2a6bcced764b737bc3afd
Sebastian <don_sebas@live.com>
2025-07-03 14:07:48 -0500

    Primer commit de CRUD Java + MySQL

---
+++ pruebaProyectoMySQL/src/pruebaproyectomysql/dbConnection.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pruebaproyectomysql;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Sebitas webitas
+ */
+public class dbConnection {
+    static String url="jdbc:mysql://127.0.0.1:3306/plataforma_acueducto";
+    static String user="root";
+    static String pass="elpepe123";
+            
+    public static Connection conectar()
+    {
+        Connection con=null;
+        try
+        {
+        con=DriverManager.getConnection(url,user,pass);
+           System.out.println("Conexion exitosa");
+        }catch(SQLException e)
+        {
+         e.printStackTrace();
+        }
+        
+        return con;
+    }
+            
+}
